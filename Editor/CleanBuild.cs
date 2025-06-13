570032444128674302c880be548e0a4ead4793d0
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-06-13 15:27:17 +0100

    Remove all Module references
    TODO: Redo the list later on so that it won't cause crashes upon startup...

--- Editor/CleanBuild.cs
+++ Editor/CleanBuild.cs
@@ -82,42 +82,7 @@ namespace Generalisk.CleanBuild.Editor
 
                     KeyValuePair<string, string>[] modules = {
                         // Package ID, DLL Name
-                        new KeyValuePair<string, string>("com.unity.modules.accessibility", "UnityEngine.AccessibilityModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.ai", "UnityEngine.AIModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.amd", "UnityEngine.AMDModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.androidjni", "UnityEngine.AndroidJNIModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.animation", "UnityEngine.AnimationModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.assetbundle", "UnityEngine.AssetBundleModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.audio", "UnityEngine.AudioModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.cloth", "UnityEngine.ClothModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.director", "UnityEngine.DirectorModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.hierarchycore", "UnityEngine.HierarchyCoreModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.imageconversion", "UnityEngine.ImageConversionModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.imgui", "UnityEngine.IMGUIModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.jsonserialize", "UnityEngine.JSONSerializeModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.nvidia", "UnityEngine.NVIDIAModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.particlesystem", "UnityEngine.ParticleSystemModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.physics", "UnityEngine.PhysicsModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.physics2d", "UnityEngine.Physics2DModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.screencapture", "UnityEngine.ScreenCaptureModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.subsystems", "UnityEngine.SubsystemsModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.terrain", "UnityEngine.TerrainModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.terrainphysics", "UnityEngine.TerrainPhysicsModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.tilemap", "UnityEngine.TilemapModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.ui", "UnityEngine.UIModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.uielements", "UnityEngine.UIElementsModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.umbra", "UnityEngine.UmbraModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unityanalytics", "UnityEngine.UnityAnalyticsModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unitywebrequestassetbundle", "UnityEngine.UnityWebRequestAssetBundleModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unitywebrequestaudio", "UnityEngine.UnityWebRequestAudioModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unitywebrequest", "UnityEngine.UnityWebRequestModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unitywebrequesttexture", "UnityEngine.UnityWebRequestTextureModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.unitywebrequestwww", "UnityEngine.UnityWebRequestWWWModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.vehicles", "UnityEngine.VehiclesModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.video", "UnityEngine.VideoModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.vr", "UnityEngine.VRModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.wind", "UnityEngine.WindModule"),
-                        new KeyValuePair<string, string>("com.unity.modules.xr", "UnityEngine.XRModule"),
+                        // TODO: Make sure that Clean Build is ran last
                     };
 
                     string managedDirectory = Application.productName + "_Data/Managed";
